ee84d4777c54fde816a05e8276976a8c8f527da1
Kai Mühlbauer <kai.muehlbauer@uni-bonn.de>
2023-01-30 09:29:42 +0100

    MNT: build

--- miubrt/io.py
+++ miubrt/io.py
@@ -61,8 +61,8 @@ def save_netcdf_dataset(ds, path, post=None, engine="netcdf4"):
     t1 = ds.time[-1].values.astype("M8[s]").astype("O") + dt.timedelta(minutes=5)
     t1 = t1.replace(second=0, microsecond=0, minute=((t1.minute // 5) * 5))
     fname = [
-        f"{ds.location.values}",
-        f"{int(round(elevation, 1)*10)}",
+        f"{ds.location.values} ",
+        f"{int(round(elevation, 1) * 10)}",
         f"{t0:%Y%m%d}",
         f"{t0:%H%M}",
         f"{t1:%H%M}",
